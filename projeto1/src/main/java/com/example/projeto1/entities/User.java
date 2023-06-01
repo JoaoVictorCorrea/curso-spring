a64b378ec1588ee0b3bef663d7ae37c9e5495788
João Victor Correa <correa.joaov25@gmail.com>
2023-06-01 13:38:00 -0300

    Create projeto1 with SPRING-DATA-JPA and MYSQL

---
+++ projeto1/src/main/java/com/example/projeto1/entities/User.java
@@ -0,0 +1,52 @@
+package com.example.projeto1.entities;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	@ManyToMany
+	private List<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
